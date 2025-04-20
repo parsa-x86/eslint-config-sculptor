93296c74e1ec5297b51868fd0e2949f610fff374
Parsa x86 <57572461+parsa-x86@users.noreply.github.com>
2025-04-20 20:54:10 +0000

    Setup TypeScript & tsup

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  outDir: "dist",
+  clean: true,
+  format: "esm",
+  target: "node18",
+  dts: true,
+  minify: true,
+  sourcemap: false,
+});
